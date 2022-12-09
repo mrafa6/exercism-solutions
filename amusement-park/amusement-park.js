c476dbabdd218e3dab49081d8d03391578e22347
mrityunjay6 <m.rafa11@outlook.com>
2022-12-09 22:43:13 +0530

    final submitted solutions

--- amusement-park/amusement-park.js
+++ amusement-park/amusement-park.js
@@ -10,7 +10,11 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  return {
+    name,
+    age,
+    ticketId,
+  };
 }
 
 /**
@@ -20,7 +24,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+  visitor.ticketId = null;
+  return visitor;
 }
 
 /**
@@ -31,7 +36,11 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+  return tickets[ticketId]
+    ? `sold to ${tickets[ticketId]}`
+    : tickets[ticketId] === null
+    ? "not sold"
+    : "unknown ticket id";
 }
 
 /**
@@ -43,7 +52,9 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
+  const ticket = tickets[ticketId];
+  if (ticket !== null && ticket !== undefined) return ticket;
+  return "invalid ticket !!!";
 }
 
 /**
@@ -53,5 +64,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  return visitor.gtc?.version;
 }
